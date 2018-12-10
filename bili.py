8059323b014ca1514dfe139fb4f5338003507e3e
Shadowmaple <1142319190@qq.com>
2018-12-10 19:54:07 +0800

    x

--- bili.py
+++ bili.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup as bs
 
 url = 'https://www.bilibili.com/v/douga/?spm_id_from=333.5.b_7072696d6172795f6d656e75.2'
 r = requests.get(url)
-soup = bs(r.text,'html.parser')
-answers = soup.find_all('div', "spread-module")
+soup = bs(r.text,'lxml')
+answers = soup.find_all('div',class_="spread-module")
 print(answers)
 for tag in answers:
     print('&')
---
+++ examples/movie_title.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from lxml import html
+import xml
+import requests
+
+url = "https://movie.douban.com/chart"
+f = requests.get(url) #Get该网页从而获取该html内容
+soup = BeautifulSoup(f.content, "lxml")
+#用lxml解析器解析该网页的内容, 好像f.text也是返回的html
+
+for k in soup.find_all('div',class_='pl2'): #找到div并且class为pl2的标签
+
+    a = k.find_all('span')
+    #在每个对应div标签下找span标签，会发现，一个a里面有四组span
+
+    print(a[0].string) #取第一组的span中的字符串
