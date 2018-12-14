930b38fb524bf5d7881cfead55562526a2914ad4
Shadowmaple <1142319190@qq.com>
2018-12-14 11:08:46 +0800

    x

---
+++ login.py
@@ -0,0 +1,27 @@
+import requests
+import getpass
+from bs4 import BeautifulSoup
+
+username = input('Uesrname：')
+password = getpass.getpass()
+
+url = 'https://account.ccnu.edu.cn/cas/login?service=http%3A%2F%2Fone.ccnu.edu.cn%2Fcas%2Flogin_portal'
+#url = "https://account.ccnu.edu.cn/cas/login"
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+cookie = r.cookies['JSESSIONID']
+lt = soup.find('input',attrs={'name':'lt'})['value']
+execution = soup.find('input',attrs={'name':'execution'})['value']
+
+payload = {
+        'username':username,
+        'password':password,
+        'lt':lt,
+        'execution':execution,
+        '_eventId':'submit',
+        'submit':'登录'
+        }
+
+url = 'https://account.ccnu.edu.cn/cas/login;jsessionid=' + cookie
+r = requests.post(url,data=payload)
+print(r.text)
