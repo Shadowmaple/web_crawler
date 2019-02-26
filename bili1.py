c9cdc29f6f4a12ec7d23068966dce87cd340bdc8
Shadowmaple <1142319190@qq.com>
2019-02-26 15:07:27 +0800

    add

---
+++ bili1.py
@@ -0,0 +1,12 @@
+#通过视频api获取视频号和标题
+
+import requests
+
+url = 'https://www.bilibili.com/index/ding.json'
+json = requests.get(url).json()
+douga = json.get('douga')
+for i in range(10):
+    av = str(douga[str(i)]['aid'])
+    title = douga[str(i)]['title']
+    print('av:'+ av + '   '+ title)
+
