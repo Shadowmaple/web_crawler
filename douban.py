a9215f1bfe6aac3e122e72dde2ddbd0a22fac776
Shadowmaple <1142319190@qq.com>
2019-12-10 11:52:41 +0800

    modify

--- douban.py
+++ douban.py
@@ -2,7 +2,10 @@ import requests
 
 url = "https://accounts.douban.com/j/mobile/login/basic"
 
-payload = "ck=03PG&name=1142319190%40qq.com&password=douban768830&remember=false&ticket="
+username = ""
+pwd = "***"
+
+payload = "ck=03PG&name=" + username + "&password=" + pwd + "&remember=false&ticket="
 headers = {
     'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0",
     'Content-Type': "application/x-www-form-urlencoded",
